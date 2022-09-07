c19154a14aa3756cb4b6bb34943db71847446bb2
jchmelevskis <109983503+jchmelevskis@users.noreply.github.com>
2022-09-07 16:26:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var Prekeskaina = +prompt("Kokia prekės kaina?")
+var Pvm = 0.21 
+var Kaina = Prekeskaina *  Pvm 
+var PrekeskainaSuPVM = (Prekeskaina + Kaina)
+
+console.log(PrekeskainaSuPVM, Kaina)
+
+
+
+
+
+
+
+var vardai = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark",]
+
+console.log(vardai[0])
+console.log(vardai[vardai.length-1])
+
+var vardai = +prompt("Įveskite skaičių nuo 1 iki 6")
+
+var vardai = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark",]
+
+console.log(vardai)
+
+
+
+
+
+// console.log(vardai[2])
+// console.log(vardai[3])
+// console.log(vardai.length)
+// console.log(vardai[vardai.length-1])
